860f26eb0e3fe2f1ea7045e4fce8d5d7f376a4f2
Fabreeeze <nandanwahi@gmail.com>
2024-12-28 23:19:52 +0530

    __init__ file corrections

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,14 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('app.config.Config')
 
     db.init_app(app)
     jwt.init_app(app)
 
     app.register_blueprint(books.bp)
     app.register_blueprint(auth.bp)
-    app.register_blueprint(members.bp)
+    app.register_blueprint(users.bp)
 
     with app.app_context():
         db.create_all()
